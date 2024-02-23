5ab8e95deeba3230590ba87c9b36f62e06c8f7cb
Quentin Serda <quentinserda@gmail.com>
2024-02-23 15:38:54 +0100

    working on iterate on h3

--- src/slices/TextSlice/index.js
+++ src/slices/TextSlice/index.js
@@ -1,45 +1,43 @@
 import {PrismicRichText} from "@prismicio/react";
 import React from "react";
+
 /**
  * @typedef {import("@prismicio/client").Content.TextSliceSlice} TextSliceSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<TextSliceSlice>} TextSliceProps
  * @param {TextSliceProps}
  */
-const TextSlice = ({ slice }) => {
-  const contentSlice = slice.items;
-/*  for (let i = 0; i < contentSlice.length; i++) {
-    let contentText = contentSlice[i];
-    if (contentText.text_item[0].type === 'heading3') {
-      console.log(contentText)
-    }
-  }
-  function f() {
-    return Array.from(contentSlice);
-  }*/
-
+const TextSlice = ({slice}) => {
+    let contentSlice = slice.items;
+    /*  for (let i = 0; i < contentSlice.length; i++) {
+        let contentText = contentSlice[i];
+        if (contentText.text_item[0].type === 'heading3') {
+          console.log(contentText)
+        }
+      }
+*/
     return (
-      <section
-          className="text"
-          data-slice-type={slice.slice_type}
-          data-slice-variation={slice.variation}
-      >
-        <div className="text__wrapper">
-            {slice.items.map((item, i) =>
-                    <PrismicRichText key={i} field={item}
-                                     components={{
-                                       paragraph: ({children}) => (
-                                           <p className="text__paragraph">{children}</p>
-                                       ),
-                                       heading3: ({children}) => (
-                                           <div className="text__titles">
-                                           </div>
-                                       )
-                                     }}
-                    >
-                    </PrismicRichText>
-            )}
-        </div>
-      </section>
+        <section
+            className="text"
+            data-slice-type={slice.slice_type}
+            data-slice-variation={slice.variation}
+        >
+            <div className="text__wrapper">
+                <PrismicRichText
+                    field={slice.primary.text_item}
+                    components={{
+                        paragraph: ({children}) => (
+                            <p className="text__paragraph">{children}</p>
+                        ),
+                        heading3: ({children}) => (
+                                <div className="titles__wrapper">
+                                    <React.Fragment> {children}</React.Fragment>
+                                </div>
+                            ),
+                    }}
+                >
+                </PrismicRichText>
+            </div>
+        </section>
     );
 };
 
